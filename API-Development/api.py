8165e011d0604b55a4a050161634f1690917cf51
Irene-Busah <i.busah@alustudent.com>
2022-11-23 03:19:47 +0200

    slight changes

--- API-Development/api.py
+++ API-Development/api.py
@@ -5,26 +5,26 @@ from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
 
 # dummy test data
-# books = [{
-# 	'id': 1,
-# 	'title': 'A Fire Upon the Deep',
-# 	'author': 'Vernor Vinge',
-#      'first_sentence': 'The coldsleep itself was dreamless.',
-#      'year_published': '1992'
-# }, {
-# 		'id': 2,
-# 		'title': 'The Ones Who Walk Away From Omelas',
-# 		'author': 'Ursula K. Le Guin',
-# 		'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-# 		'published': '1973'},
-# 	 {
-# 		'id': 3,
-# 		'title': 'Dhalgren',
-# 		'author': 'Samuel R. Delany',
-# 		'first_sentence': 'to wound the autumnal city.',
-# 		'published': '1975'
-# 	 }
-# ]
+books = [{
+	'id': 1,
+	'title': 'A Fire Upon the Deep',
+	'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'
+}, {
+		'id': 2,
+		'title': 'The Ones Who Walk Away From Omelas',
+		'author': 'Ursula K. Le Guin',
+		'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+		'published': '1973'},
+	 {
+		'id': 3,
+		'title': 'Dhalgren',
+		'author': 'Samuel R. Delany',
+		'first_sentence': 'to wound the autumnal city.',
+		'published': '1975'
+	 }
+]
 
 def dict_factory(cursor, row):
 	dictionary = {}
